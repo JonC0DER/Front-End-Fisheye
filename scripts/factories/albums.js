5b62c3701b84783d213248ba475d2bc4b97477ff
joncoder <joncoder.kamite@gmail.com>
2022-03-09 15:01:23 +0100

    creation de la galerie photos videos par photographe

---
+++ scripts/factories/albums.js
@@ -0,0 +1,40 @@
+function albumFactory(data, name, identifiant) {
+    const {id, photographerId, title, image, video, likes, date, price} = data;
+    const path = `assets/albums/${name}`;
+
+    function getUserAlbumDOM(){
+        if (photographerId == identifiant) {
+            const figure = document.createElement('figure');
+            figure.className = `photo_video_album ${id}`;
+            figure.setAttribute('data-date', date)
+            if (video == undefined) {
+                const img = document.createElement('img');
+                img.setAttribute('src', `${path}/${image}`);
+                figure.appendChild(img);
+            }else{
+                const videoTag = document.createElement('video');
+                videoTag.setAttribute('src', `${path}/${video}`);
+                //videoTag.setAttribute('controls', '');
+                figure.appendChild(videoTag);
+            }
+            const figcaption = document.createElement('figcaption');
+            const h3 = document.createElement('h3');
+            h3.className = 'title';
+            h3.textContent = title;
+            const p = document.createElement('p');
+            p.className = 'likes-content';
+            const span = document.createElement('span');
+            span.className = 'likes';
+            span.textContent = likes;
+            const i = document.createElement('i');
+            i.className = 'fas fa-heart';
+            p.appendChild(span);
+            p.appendChild(i);
+            figcaption.appendChild(h3);
+            figcaption.appendChild(p);
+            figure.appendChild(figcaption);
+            return (figure);           
+        }
+    }
+    return {getUserAlbumDOM}
+}
