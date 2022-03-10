189fa4d307f1bdc1741e459b88a1baf79b173fee
joncoder <joncoder.kamite@gmail.com>
2022-03-10 11:40:37 +0100

    ajout de la fonction submitForm()

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,18 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+function submitForm() {
+    const form = document.forms[0];
+    const sendBtn = form["elements"][4];
+    const formArray = Array.from(form['elements']);
+
+    sendBtn.addEventListener('click', (event)=>{
+        event.preventDefault();
+        formArray.forEach(input => {
+            if (input.tagName == 'INPUT' || input.tagName == 'TEXTAREA') {
+                console.log(`${input.previousElementSibling.textContent} : ${input.value}`);
+            }
+        });
+    })
+}
