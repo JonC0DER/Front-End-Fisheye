c66cf6e330c19f0e62af003c15d33202be69e328
joncoder <joncoder.kamite@gmail.com>
2022-04-15 18:26:27 +0200

    creation d'un custom selectpour le graphisme

---
+++ scripts/factories/customSelect.js
@@ -0,0 +1,31 @@
+function customSelectFactory(select) {
+    function buildCustomSelect() {
+        const customContainer = document.createElement('div');
+        customContainer.classList.add('trie_fake');
+        
+        const customValue = document.createElement('div');
+        customValue.classList.add('select-options', 'select-arrow-up', 'select-options-border-bottom');
+        customValue.textContent = firsLetterUpperCase(select.value);
+
+        const customOptions = document.createElement('div');
+        customOptions.classList.add('select-items', 'select-items-hidden');
+
+        const selectArray = Array.from(select.children);
+        selectArray.forEach(options => {
+            const customOpt = document.createElement('div');
+            customOpt.classList.add('list-item');
+            customOpt.textContent = options.value;
+            if (options.value === select.value) {
+                customOpt.classList.add('select-items-hidden');
+            }
+            customOptions.appendChild(customOpt);
+        })
+
+        customContainer.appendChild(customValue);
+        customContainer.appendChild(customOptions);
+
+        return (customContainer);
+    }
+
+    return {buildCustomSelect}
+}
