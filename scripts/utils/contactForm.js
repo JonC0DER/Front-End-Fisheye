4e7ec12ea0f62ea795c08debca944727f953b007
joncoder <joncoder.kamite@gmail.com>
2022-05-31 12:15:25 +0200

    refactoring du fichier et du btn d'envoi formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,25 +1,34 @@
+const modal = document.getElementById("contact_modal");
+const form = document.forms[0];
+let contactListener = null;
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+    const formContainer = modal.children[0];
     modal.firstElementChild.setAttribute('aria-labelledby', 'contact_photographer');
 	modal.style.display = "flex";
+    //contactListener = keyCodeListener(form['elements'], formContainer);
+
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    if (contactListener !== null) {
+        const contactArray = contactListener.getContentArray();
+        console.log(contactArray[0]);
+        console.log(contactArray[1]);
+        contactListener.removeListener();
+        contactListener = null;   
+    }
 }
 
-function submitForm() {
-    const form = document.forms[0];
-    const sendBtn = form["elements"][4];
+form['elements'][4].addEventListener('click', function (event) {
     const formArray = Array.from(form['elements']);
-
-    sendBtn.addEventListener('click', (event)=>{
-        event.preventDefault();
-        formArray.forEach(input => {
-            if (input.tagName == 'INPUT' || input.tagName == 'TEXTAREA') {
+    formArray.forEach(input => {
+        if ((input.tagName == 'INPUT' || input.tagName == 'TEXTAREA') && input.value !== '') {
+            if (input.type !== 'submit') {   
                 console.log(`${input.previousElementSibling.textContent} : ${input.value}`);
             }
-        });
-    })
-}
+        }
+    });
+    event.preventDefault();
+}, false);
