25e64f5c269c08dafbf5da1176f82b389ed5d394
joncoder <joncoder.kamite@gmail.com>
2022-07-26 12:52:03 +0200

    refactoring de closeupview.js supression prochaine du fichier

--- scripts/factories/closeupView.js
+++ scripts/factories/closeupView.js
@@ -3,31 +3,13 @@
  */
 function closeupViewFactory() {
     const album = document.querySelector('.album');
-    if (album !== null) {
-        const cross = document.createElement('span');
-        cross.className = 'close_icon';
-        cross.setAttribute('onclick', 'closeLightBox()');
-        cross.setAttribute('role', 'button');
-        cross.setAttribute('tabindex', 0);
-        cross.setAttribute('aria-label', 'Close dialog');
-        const previous = document.createElement('div');
-        previous.setAttribute('aria-label', 'previous image');
-        previous.setAttribute('role', 'link');
-        previous.className = "previous";
-        const next = document.createElement('div');
-        next.setAttribute('aria-label', 'next image');
-        next.setAttribute('role', 'link');
-        next.className = "next";
-        album.appendChild(cross);
-        album.insertBefore(previous, album.childNodes[0]);
-        album.appendChild(next);
-        album.className = "lightBox";
-        album.setAttribute('aria-label', 'image closeup view');
-        album.setAttribute('role', 'dialog');
+    album.classList.add('dsp-none');
+    album.classList.remove('dsp-flex');
+        
+    const lightBox = document.querySelector('.lightBox');
+    lightBox.classList.add('dsp-flex');
+    lightBox.classList.remove('dsp-none');
 
-        if (album.className == 'lightBox') {
-            initVideo();
-            listenCloseupViewNavigation();
-        }
-    }
+    initVideo();
+    listenCloseupViewNavigation();
 }
